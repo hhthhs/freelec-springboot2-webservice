ee1fe386c65d567a12ba3126c41fd4df45d2160a
sezero <dud832@gmail.com>
2022-03-10 15:44:24 +0900

    3.10 162p

--- src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepositoy.java
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepositoy.java
@@ -1,7 +1,13 @@
 package com.jojoldu.book.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PostsRepositoy extends JpaRepository<Posts,Long> {
+
+    @Query("SELECT p FROM Posts p ORDER BY [.id DESC")
+    List<Posts> findAllDesc();
 
 }
